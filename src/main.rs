2c2df446403674af92ca8ad2836bbc48d164c9c4
keyrole <yijianan1426@163.com>
2022-07-24 14:15:28 +0800

    finish single thread version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::fs;
+use std::time;
+use std::thread;
 
-fn main() {
-    println!("hi, good");
+// struct Worker {
+
+//     id: usize,
+
+// }
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let a = b"GET / HTTP/1.1\r\n";
+    let (status_line, file_name) = if buffer.starts_with(a) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "main.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let content = fs::read_to_string(file_name).unwrap();
+    let response = format!("{}{}", status_line, content);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    let te = time::Duration::from_millis(10000);
+    thread::sleep(te);
+}
+
+fn main() -> std::io::Result<()>{
+    let listen = TcpListener::bind("127.0.0.1:8080")?;
+    for stream in listen.incoming() {
+        handle_stream(stream?);
+    }
+
+    Ok(())
 }
