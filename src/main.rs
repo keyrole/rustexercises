3132d64c0f36d8eb3a2eb1b714f7d45e0067311a
keyrole <yijianan1426@163.com>
2022-07-28 11:25:04 +0800

    learn how to use atomic type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use std::{sync::atomic::{AtomicU64, Ordering}, time::Instant, thread::{JoinHandle, self}, ops::Sub};
 
+const N_TIMES: u64 = 10000000;
+const N_THREADS: usize = 10;
+
+static R: AtomicU64 = AtomicU64::new(0);
+
+fn add_n_times(n_times: u64) -> JoinHandle<()> {
+    thread::spawn(move || {
+        for _ in 0..n_times {
+            R.fetch_add(1, Ordering::Relaxed);
+        }
+    })
+}
 fn main() {
-    println!("hi, good");
+    let s = Instant::now();
+    let mut threads = Vec::with_capacity(N_THREADS);
+
+    for _ in 0..N_THREADS {
+        threads.push(add_n_times(N_TIMES));
+    }
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+
+    assert_eq!(N_TIMES * N_THREADS as u64, R.load(Ordering::Relaxed));
+
+    println!("{:?}", Instant::now().sub(s));
 }
