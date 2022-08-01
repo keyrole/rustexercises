47a69727cc96db5697fcbda1ddbaf03c688a4063
keyrole <yijianan1426@163.com>
2022-08-01 16:34:10 +0800

    learn hmac-sha256

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::{Read, BufReader}};
 use error_chain::error_chain;
 use data_encoding::HEXUPPER;
-use ring::digest::{Context, Digest, SHA256};
+use ring::{digest::{Context, Digest, SHA256}, rand::{self, SecureRandom}, hmac};
 
 error_chain! {
     foreign_links {
@@ -15,6 +15,7 @@ fn main() -> Result<()> {
     let reader = BufReader::new(input);
     let digest = sha256_digest(reader)?;
     println!("{}", HEXUPPER.encode(digest.as_ref()));
+    hmac_verify()?;
     Ok(())
 }
 
@@ -35,4 +36,18 @@ fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
         context.update(&buffer[..count]);
     }
     Ok(context.finish())
+}
+
+fn hmac_verify() -> Result<()> {
+    let mut key_value = [0u8; 48];
+    let rng = rand::SystemRandom::new();
+    rng.fill(&mut key_value).unwrap();
+    let key = hmac::Key::new(hmac::HMAC_SHA256, &key_value);
+
+    let message = "this is a test";
+    let signature= hmac::sign(&key, message.as_bytes());
+    if hmac::verify(&key, message.as_bytes(), signature.as_ref()).is_ok() {
+        println!("Verify pass: {signature:?} is the hmac result of {message}")
+    }
+    Ok(())
 }
